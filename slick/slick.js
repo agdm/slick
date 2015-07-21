653e8a1549a2cde57852cbe897910359ff6d3e36
Alvin Milton <alvinm@complex.com>
2015-07-21 12:07:03 -0400

    updating repo refs

--- slick/slick.js
+++ slick/slick.js
@@ -10,8 +10,8 @@
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
-    Repo: http://github.com/kenwheeler/slick
-  Issues: http://github.com/kenwheeler/slick/issues
+    Repo: http://github.com/agdm/slick
+  Issues: http://github.com/agdm/slick/issues
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
